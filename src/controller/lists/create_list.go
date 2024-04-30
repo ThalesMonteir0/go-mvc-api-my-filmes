e496ef8c4ff4e972eb3cfd21265ddb9797198bba
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2024-04-29 22:10:06 -0300

    ajustes

--- src/controller/lists/create_list.go
+++ src/controller/lists/create_list.go
@@ -10,15 +10,13 @@ import (
 func (lc *listController) CreateList(c *fiber.Ctx) error {
 	var listBody request.ListRequest
 
-	errParse := c.BodyParser(&listBody)
-	if errParse != nil {
+	if err := c.BodyParser(&listBody); err != nil {
 		errRequest := rest_err.NewBadRequestError("struct error")
 		return c.Status(errRequest.Code).JSON(errRequest.Message)
 	}
 
 	listDomain := model.NewListDomain(listBody.Title, listBody.Description, listBody.UserID)
-	err := lc.service.CreateList(listDomain)
-	if err != nil {
+	if err := lc.service.CreateList(listDomain); err != nil {
 		return c.Status(err.Code).JSON(err.Message)
 	}
 
